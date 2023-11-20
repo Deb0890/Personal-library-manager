310c4d7db151a80325a5b2d080b7fa89f9730487
deb0890 <deborahf1@hotmail.com>
2023-11-20 15:09:23 +0000

    bundler and compiler setup, dependencies, initial folder setup, hello world

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// index.js
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./style.css";
+
+const container = document.getElementById("app");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
