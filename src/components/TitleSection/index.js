fbed8942cf75195fb9f41fcc1056e4e40199cf77
Deb0890 <deborahf1@hotmail.com>
2023-12-13 08:31:20 +0000

    change title component to title section

---
+++ src/components/TitleSection/index.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import BookForm from "../BookForm";
+
+const TitleSection = () => {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  return (
+    <div id="title-section">
+      <h2 className="title">The Library</h2>
+      <button id="add-a-book" onClick={toggleModal}>
+        Add a Book
+      </button>
+      {modal && <BookForm />}
+      <hr />
+    </div>
+  );
+};
+
+export default TitleSection;
