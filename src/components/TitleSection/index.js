ea99ff06c95e746759723e92a806aeb9e4b5e767
Deb0890 <deborahf1@hotmail.com>
2024-02-10 13:49:18 +0000

    delete separate bookform component and move into titlesection

--- src/components/TitleSection/index.js
+++ src/components/TitleSection/index.js
@@ -1,11 +1,32 @@
-import React, { useState } from "react";
-import BookForm from "../BookForm";
+import React, { useState, useEffect } from "react";
 
 const TitleSection = () => {
   const [modal, setModal] = useState(false);
+  const [formData, setFormData] = useState({
+    booktitle: "",
+    authorfirstname: "",
+    authorlastname: "",
+    image: "",
+    genreone: "",
+    genretwo: "",
+  });
 
   const toggleModal = () => {
     setModal(!modal);
+    setFormData({
+      booktitle: "",
+      authorfirstname: "",
+      authorlastname: "",
+      image: "",
+      genreone: "",
+      genretwo: "",
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    toggleModal();
+    console.log(formData);
   };
 
   return (
@@ -14,7 +35,109 @@ const TitleSection = () => {
       <button id="add-a-book" onClick={toggleModal}>
         Add a Book
       </button>
-      {modal && <BookForm closeModal={toggleModal} />}
+      {modal && (
+        <div className="modal">
+          <div className="modal-content">
+            <h3>Add a Book</h3>
+            <form
+              action="POST"
+              className="form"
+              id="main-form"
+              onSubmit={handleSubmit}
+            >
+              <div className="form-group">
+                <label htmlFor="form-book-title">Book title</label>
+                <input
+                  type="text"
+                  id="form-book-title"
+                  onChange={(e) => {
+                    setFormData({ ...formData, booktitle: e.target.value });
+                  }}
+                  value={formData.booktitle}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="form-author-fn">Author FIRST NAME</label>
+                <input
+                  type="text"
+                  id="form-author-fn"
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      authorfirstname: e.target.value,
+                    });
+                  }}
+                  value={formData.authorfirstname}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="form-author-ln">Author LAST NAME</label>
+                <input
+                  type="text"
+                  id="form-author-ln"
+                  onChange={(e) => {
+                    setFormData({
+                      ...formData,
+                      authorlastname: e.target.value,
+                    });
+                  }}
+                  value={formData.authorlastname}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="form-upload-img"></label>
+                <input
+                  type="file"
+                  id="form-upload-img"
+                  placeholder="Upload an image of your copy:"
+                  onChange={(e) => {
+                    setFormData({ ...formData, image: e.target.value });
+                  }}
+                  value={formData.image}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="form-genre-one">Genre 1</label>
+                <select
+                  type="select"
+                  id="form-genre-one"
+                  onChange={(e) => {
+                    setFormData({ ...formData, genreone: e.target.value });
+                  }}
+                  value={formData.genreone}
+                >
+                  <option value="chooseAGenre">Choose a genre</option>
+                  <option value="genreOne">genre one</option>
+                  <option value="genreTwo">genre two</option>
+                </select>
+              </div>
+              <div className="form-group">
+                <label htmlFor="form-genre-one">Genre 2</label>
+                <select
+                  type="select"
+                  id="form-genre-two"
+                  onChange={(e) => {
+                    setFormData({ ...formData, genretwo: e.target.value });
+                  }}
+                  value={formData.genretwo}
+                >
+                  <option value="chooseASecondaryGenre">
+                    Choose a secondary genre (Optional)
+                  </option>
+                  <option value="genreOne">genre one</option>
+                  <option value="genreTwo">genre two</option>
+                </select>
+              </div>
+            </form>
+            <button form="main-form" onClick={toggleModal}>
+              Cancel
+            </button>
+            <button form="main-form" type="submit">
+              Save
+            </button>
+          </div>
+        </div>
+      )}
       <hr />
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,2 @@
 export { default as Header } from "./Header";
 export { default as TitleSection } from "./TitleSection";
-export { default as BookForm } from "./BookForm";
