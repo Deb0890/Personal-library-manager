6f4f94924235c79721ad2d5218cb0838d2335685
Deb0890 <deborahf1@hotmail.com>
2024-02-19 00:32:42 +0000

    add component functions to empty files and export where needed

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-
 import { AddBook } from "../../components";
 
 const Books = () => {
---
+++ src/pages/Home/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Home = () => {
+  return <div>Hi, your at the homepage!</div>;
+};
+
+export default Home;
