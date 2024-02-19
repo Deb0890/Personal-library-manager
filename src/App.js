85df11d5e3cf982c6289e962e9fb5bb3e05f0d12
Deb0890 <deborahf1@hotmail.com>
2024-02-19 00:26:45 +0000

    add and install routing tags

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Header } from "./components";
-import { Books } from "./pages";
+import { Books, Home } from "./pages";
 
 const App = () => {
   return (
     <>
       <Header />
-      <Books />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/books" element={<Books />} />
+      </Routes>
     </>
   );
 };
