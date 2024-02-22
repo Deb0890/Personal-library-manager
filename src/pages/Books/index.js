afaf4933d8b91b1fd4282c9b23f5ee6ab3f96d5b
Deb0890 <deborahf1@hotmail.com>
2024-02-22 23:11:41 +0000

    set up books parent page

--- src/pages/Books/index.js
+++ src/pages/Books/index.js
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { AddBook } from "../../components";
 
 const Books = () => {
-  return <AddBook />;
+  const [booksPresent, setBooksPresent] = useState(false);
+  const message =
+    "You currently have no books in your library. Add a book to get started.";
+  return (
+    <>
+      <AddBook />
+      {booksPresent ? (
+        <a href="">
+          <div className="book-card">book details</div>
+        </a>
+      ) : (
+        <div className="no-books-yet">{message}</div>
+      )}
+    </>
+  );
 };
 
 export default Books;
+//The message should only show when there is no book data present to retrieve
+//If there are books present they are retrieved and displayed
+//If no books, display message. Otherwise don't display message, display book.
+//Does the message need to be in state? Probably not but should be in some kind of variable...
+
+//On initial render, API is called to check for books.
+//books? displayBooks : message;
